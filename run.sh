2a21da918e8205fe79eb779bdc2ca41dcfac62a5
Solarnode™️ [DEV] <m1lkyt.republic@gmail.com>
2024-11-12 23:27:21 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 printf "\033c"
-printf "${GREEN}Starting..${NC}\n"
+printf "${GREEN}[PteroVPS] Starting Up..${NC}\n"
 sleep 1
 printf "\033c"
 
@@ -17,15 +17,17 @@ if [ "$PWD" = "/root"* ]; then
 fi
 
 printf "\033c"
-printf "${GREEN}╭────────────────────────────────────────────────────────────────────────────────╮${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                             Pterodactyl VPS EGG                                │${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                           ${RED}© 2021 - 2024 ${PURPLE}ysdragon${GREEN}                               │${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}╰────────────────────────────────────────────────────────────────────────────────╯${NC}\n"
+printf "${GREEN}=============================${NC}"
+printf "${GREEN}Welcome to PteroVPS 2024!${NC}"
+printf "${GREEN}You are up-to-date running 1.0.${NC}"
+printf "${GREEN}Developed by solarnode | forked @ ysdragon${NC}"
+printf "${GREEN}=============================${NC}"
+sleep 1
+printf "${GREEN}=============================${NC}"
+printf "${GREEN}Status: Active${NC}"
+printf "${GREEN}=============================${NC}"
 printf "                                                                                               \n"
-printf "root@MyVPS:${DIR}#                                                                             \n"
+printf "root@MyVPS:${DIR}#                                                                             \n"                                                                             
 
 run_cmd() {
     read -p "root@MyVPS:$DIR# " CMD
